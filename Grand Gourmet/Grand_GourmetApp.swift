8fcf552deb6f964ab4ce3f50b946c6a6709f498c
mryusuf <muhamadyusufindra@gmail.com>
2022-06-01 17:26:57 +0700

    Initial Commit

---
+++ Grand Gourmet/Grand_GourmetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grand_GourmetApp.swift
+//  Grand Gourmet
+//
+//  Created by Indra Permana on 01/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Grand_GourmetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
